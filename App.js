9f2d9c2360d8ad1464d2541091e5576cdd837cbc
Kylebbos <kasperikuusisto@gmail.com>
2024-02-06 17:55:42 +0200

    Updated

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React, { useState } from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Calculator from "./components/Calculator";
+import History from "./components/History";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [history, setHistory] = useState([]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Calculator') {
+              iconName = focused ? 'calculator' : 'calculator';
+            } else if (route.name === 'History') {
+              iconName = focused ? 'list' : 'list';
+            }
+
+            return <Ionicons name={iconName} size={size} color="pink" />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Calculator">
+          {() => <Calculator setHistory={setHistory} history={history} />}
+        </Tab.Screen>
+        <Tab.Screen name="History">
+          {() => <History history={history} />}
+        </Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
